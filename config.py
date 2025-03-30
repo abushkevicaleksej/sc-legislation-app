073e2add2ff584537e332930fb835eb0d95bfe91
alex <abuskevicaleksej@gmail.com>
2025-03-30 11:18:04 +0300

    config edited

--- config.py
+++ config.py
@@ -9,3 +9,9 @@ class Config:
         "auth_agent": "service.agents.mock.OstisAuthAgent",
     }
     OSTIS_URL = config['DEFAULT']['ostis_url']
+    PROTOCOL = config['SERVER']['SC_SERVER_PROTOCOL']
+    HOST = config['SERVER']['SC_SERVER_HOST']
+    PORT = config['SERVER']['SC_SERVER_PORT']
+    PROTOCOL_DEFAULT = config['SERVER']['SC_SERVER_PROTOCOL_DEFAULT']
+    HOST_DEFAULT = config['SERVER']['SC_SERVER_HOST_DEFAULT']
+    PORT_DEFAULT = config['SERVER']['SC_SERVER_PORT_DEFAULT']
