49f450bff5456ae4890a79d0ffc1229a1c38de4a
alex <abuskevicaleksej@gmail.com>
2025-03-30 12:28:19 +0300

    html fixed and added to routes

--- service/views.py
+++ service/views.py
@@ -15,7 +15,22 @@ def index():
 def auth():
     return render_template('authorization.html')
 
-
 @main.route("/reg")
 def reg():
     return render_template('registration.html')
+
+@main.route("/request")
+def request():
+    return '200 ok'
+
+@main.route("/directory")
+def directory():
+    return 'directory route'
+
+@main.route("/templates")
+def templates():
+    return 'templ route'
+
+@main.route("/calendar")
+def calendar():
+    return 'calendar route'
